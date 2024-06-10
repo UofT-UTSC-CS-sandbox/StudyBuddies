ab2ecc54e4f58b723289c20d5bd238090c8ba199
Damian Melia <meliadamian17@gmail.com>
2024-06-10 13:58:00 -0400

    starting sign up / login

--- src/server/handlers/handler.go
+++ src/server/handlers/handler.go
@@ -1,1 +1,29 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/VibeMerchants/StudyBuddies/model"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	userService model.UserService
+}
+
+type Config struct {
+	R           *gin.Engine
+	UserService model.UserService
+}
+
+func NewHandler(c *Config) {
+	h := &Handler{
+		userService: c.UserService,
+	}
+	c.R.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Not Found: Invalid API Route"})
+	})
+
+	accGroup := c.R.Group("/api/account")
+
+}
