9edf04d6225a115a0926d0d5970c58aad4ee2351
BrandanBurgess <brandanbb13@gmail.com>
2024-06-14 13:23:51 -0400

    fix: Made map home  screen

---
+++ src/client/StudyBuddies/app/(tabs)/account.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+//I want to return basic <p> Hello world </p> to test if the screen is working
+//I will replace this with the actual map component later
+const AccountScreen = () => {
+  return (
+    <text> Hello world </text>
+  );
+};
+
+
+
+
+export default AccountScreen;
+
