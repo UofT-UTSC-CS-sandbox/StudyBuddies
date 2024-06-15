cad7dd7224640e4a93cfa44bb78c0a9acf7396cc
BrandanBurgess <brandanbb13@gmail.com>
2024-06-14 21:55:47 -0400

    feat: add markers to map

--- src/client/StudyBuddies/components/GoogleMapsScreen.tsx
+++ src/client/StudyBuddies/components/GoogleMapsScreen.tsx
@@ -9,7 +9,7 @@ import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 const styles = StyleSheet.create({
     container: {
@@ -29,9 +29,60 @@ const styles = StyleSheet.create({
     return (
       <View style={styles.container}>
         <MapView 
+          
           style={styles.map} 
-        />
+          region={{
+            latitude: 43.841420,
+            longitude: -79.003680,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          >
+      
+          <Marker
+            coordinate={{
+              latitude: 43.841420,
+              longitude: -79.003680,
+            }}
+            title="Current Location"
+            description="This is where you are."
+          />
+
+          <Marker
+            coordinate={{
+              latitude: 43.786770,
+              longitude: -79.189580,
+            }}
+            title="IC Building"
+            description="Where the academic weapons come to work."
+          />
+
+
+          <Marker
+            coordinate={{
+              latitude: 43.783900,
+              longitude: -79.186290,
+            }}
+            title="Library"
+            description="Book a day keeps the bad grades away."
+          />
+
+
+          <Marker
+            coordinate={{
+              latitude: 43.790901,
+              longitude: -79.194283,
+            }}
+            title="Pan Am Centre"
+            description="Come Ball Up Fam."
+          />
+
+  
+      
+        </MapView>
       </View>
     );
   
   }
+
+  
