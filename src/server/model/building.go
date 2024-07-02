611e9d0dd51c8a708cb8cd9770afb24fd4d1301f
Amayu <AshwinM0304@gmail.com>
2024-07-01 21:44:28 -0400

    KAN-59: Created Model for groups, and added/updated models neccesary for groups

--- src/server/model/building.go
+++ src/server/model/building.go
@@ -2,6 +2,8 @@ package model
 
 type Building struct {
 	DefaultModel
-	Name  string `gorm:"not null;uniqueIndex" json:"name"`
-	Image string `json:"image"`
+	Name         string `gorm:"not null;uniqueIndex" json:"name"`
+	Image        string `json:"image"`
+	BuildingCode string `gorm:"not null;uniqueIndex" json:"code"`
+	Rooms        []Room `gorm:"many2many:rooms;"`
 }
