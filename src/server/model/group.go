7a4fafcb1655b16326d810f1f9cf80d5a486b45d
Amayu <AshwinM0304@gmail.com>
2024-07-02 20:58:34 -0400

    Updating Group Model to reflect group chat functionality

--- src/server/model/group.go
+++ src/server/model/group.go
@@ -2,11 +2,8 @@ package model
 
 type Group struct {
 	DefaultModel
-	Name        string   `gorm:"not null;uniqueIndex" json:"name"`
-	Description string   `json:"description"`
-	Courses     []Course `gorm:"many2many:group_courses;"`
-	Owner       User     `gorm:"many2one:owner;"`
-	Members     []User   `gorm:"many2many:group_members;"`
-	Building    Building `gorm:"many2one:building;"`
-	Room        Room     `gorm:"many2one:room;"`
+	Name        string `gorm:"not null;uniqueIndex" json:"name"`
+	Description string `json:"description"`
+	Owner       User   `gorm:"many2one:owner;"`
+	Members     []User `gorm:"many2many:group_members;"`
 }
