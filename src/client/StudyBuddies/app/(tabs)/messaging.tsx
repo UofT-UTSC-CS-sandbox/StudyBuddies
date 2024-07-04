632202c3c6e9c0482c1c96d200f6be8529b4a30b
gokhalechinmay <gokhalechinmay4@gmail.com>
2024-07-04 19:57:17 -0400

    small changes with imports

--- src/client/StudyBuddies/app/(tabs)/messaging.tsx
+++ src/client/StudyBuddies/app/(tabs)/messaging.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from '/Users/cgokhale/Desktop/chinmay/utsc_3year/c01/StudyBuddies/src/client/StudyBuddies/store';
+import { store, persistor } from '../../store';
+
 import AppNavigator from '@/components/AppNavigator';
 
 const ChatListScreen1 = () => {
